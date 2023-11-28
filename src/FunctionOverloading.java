95ed38ef7a0ad93a83a202535b6b064e34ea85d7
biplab-w <waglebip@gmail.com>
2023-11-28 14:36:45 -0600

    Added print methods

--- src/FunctionOverloading.java
+++ src/FunctionOverloading.java
@@ -2,4 +2,15 @@ public class FunctionOverloading {
     public static void main(String[] args) {
 
     }
+    public void print(String message) {
+        System.out.println("Printing message: " + message);
+    }
+
+    public void print(String[] messages) {
+        String allMessages ="";
+        for(String message : messages){
+            System.out.println(str += " ");
+        }
+        System.out.println("Printing messages: " + allMessages);
+    }
 }
